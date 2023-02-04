cdf1454be67960d0e5453e26954cdaddfdf97334
Manuel De La Rosa <manueldelarosa9722@gmail.com>
2023-02-04 00:18:31 -0600

    Created a header and added it to base.html

--- blogger/user/views.py
+++ blogger/user/views.py
@@ -15,32 +15,6 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('home')
 
 
-# class RegisterView(FormView):
-#     form_class = RegisterForm
-#     template_name = 'register.html'
-#     success_url = reverse_lazy('home')
-
-    # def get(self, request, *args, **kwargs):
-    #     form = self.form_class(initial=self.initial)
-
-    #     return render(request, self.template_name, {'form': form})
-
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-
-    #     if form.is_valid():
-    #         first_name = form.cleaned_data.get('first_name')
-    #         last_name = form.cleaned_data.get('last_name')
-    #         username = form.cleaned_data.get('username')
-    #         email = form.cleaned_data.get('email')
-    #         password = form.cleaned_data.get('password')
-
-    #         form.save()
-    #         return HttpResponseRedirect(reverse_lazy('home'))
-
-    #     return render(request, self.template_name, {'form': form})
-
-
 class ProfileView(DetailView):
     model = User
     template_name = 'profile.html'
