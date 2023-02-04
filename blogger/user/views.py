8937f67c1bee0b99a423f3e73f0d58fb687838a1
Manuel De La Rosa <manueldelarosa9722@gmail.com>
2023-02-04 12:09:36 -0600

    Created profile page and added some css

--- blogger/user/views.py
+++ blogger/user/views.py
@@ -5,7 +5,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
 from .forms import RegisterForm
-from .models import User
+from .models import User, UserInfo
 
 
 class RegisterView(CreateView):
@@ -19,6 +19,12 @@ class ProfileView(DetailView):
     model = User
     template_name = 'profile.html'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_info'] = UserInfo.objects.get(user=self.object)
+        context['display_options'] = ['email', 'dob', 'sex', 'active']
+        return context
 
 
 class IndexView(TemplateView):
